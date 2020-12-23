7db9273bfea2b06a7f93b1df754c5ecd8c5d5c9e
Jovan Zoric <jovan@example.com>
2020-12-23 20:37:28 +0100

    zavrseno sve do 5og zadatka.

---
+++ single_post.php
@@ -0,0 +1,45 @@
+<?php  include ('connection.php')?>
+
+
+<html>
+    <?php
+    $sqlComments ="SELECT * FROM posts JOIN comments ON comments.post_id = post.id WHERE comments.post_id = {$_GET['post_id']}";
+    $statement = $connection->prepare($sqlComments);
+    $statement->execute();
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    $comments = $statement->fetchAll();
+    ?>
+    <ul>
+        <?php
+        foreach ($comments as $comment) {
+        ?>
+        <li style="list-style-type: none">
+            <p class="blog-post-meta" <?php echo($comment['id']) ?>>Comment by: <a href="#"><?php echo($comment['author'])?></a></p>
+            <p><?php echo($comment['text'])?></p>
+        </li>
+        <?php
+        }
+        ?>
+    </ul>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
